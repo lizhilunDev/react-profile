9708818552b70023fcf60d5e0c679a8bf390d1db
jiyoonlee <im.lizhilun@gmail.com>
2024-03-04 02:16:27 +0900

    AboutMe 페이지 생성

---
+++ src/pages/section/AboutMe.js
@@ -0,0 +1,12 @@
+import Section from "../../components/Section";
+import MovingCard from "../../components/MovingCard";
+
+const AboutMe = () => {
+  return (
+    <>
+      <Section title={"About Me."} content={<MovingCard />} />
+    </>
+  );
+};
+
+export default AboutMe;
