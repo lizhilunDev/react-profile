68f41f9244bc18499ff67422e87751a2520a94c8
jiyoonlee <im.lizhilun@gmail.com>
2024-03-03 20:21:40 +0900

    MainTitle.js 및 scss 생성

---
+++ src/pages/main/MainTitle.js
@@ -0,0 +1,16 @@
+import "../../styles/pages/main/MainTitle.css";
+
+const MainTitle = () => {
+  return (
+    <>
+      <div class='title-wrap'>
+        <h1>
+          <sapn>Front-end</sapn>
+          <span>Jiyoon Lee</span>
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default MainTitle;
