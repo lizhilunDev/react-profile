fca174be2e3c1be9d2de1a7822ae12dfcbbcc57e
jiyoonlee <im.lizhilun@gmail.com>
2024-03-07 03:45:09 +0900

    skillIndex > index 변경

--- src/components/SkillContainer.js
+++ src/components/SkillContainer.js
@@ -1,14 +1,14 @@
-import React from "react";
-import SkillCard from "./SkillCard";
-import "../styles/components/SkillContainer.css";
+import React from 'react';
+import SkillCard from './SkillCard';
+import '../styles/components/SkillContainer.css';
 
 const SkillContainer = ({ skills }) => {
   return (
     <>
       <div className='skill-container'>
-        {skills.map((skill, skillIndex) => (
+        {skills.map((skill, index) => (
           <SkillCard
-            key={`skill_${skillIndex}`}
+            key={`skill_${index}`}
             imgSrc={skill.imgSrc}
             imgAlt={skill.imgAlt}
             title={skill.title}
