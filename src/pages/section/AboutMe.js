a35e168fab2a5f4ac6c478f3403f63fb216c1ede
jiyoonlee <im.lizhilun@gmail.com>
2024-03-09 03:44:27 +0900

    이미지 교체

--- src/pages/section/AboutMe.js
+++ src/pages/section/AboutMe.js
@@ -1,17 +1,5 @@
-import Section from '../../components/Section';
-import MovingCard from '../../components/MovingCard';
-
 const AboutMe = () => {
-  return (
-    <>
-      <Section
-        id='about'
-        title={'About Me.'}
-        content={<MovingCard />}
-        description='카드 위에서 마우스를 움직여보세요!'
-      />
-    </>
-  );
+  return <></>;
 };
 
 export default AboutMe;
