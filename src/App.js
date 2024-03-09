3aa304eaf8e37f2b292880b312e5ddb7d71c31d9
jiyoonlee <im.lizhilun@gmail.com>
2024-03-10 01:40:19 +0900

    reset.css > styled-reset 으로 변경

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Reset } from 'styled-reset';
 import Gnb from './components/Gnb/Gnb';
 import Page from './pages/Page';
 import React from 'react';
@@ -5,6 +6,7 @@ import React from 'react';
 function App() {
   return (
     <>
+      <Reset />
       <Gnb></Gnb>
       <Page></Page>
     </>
