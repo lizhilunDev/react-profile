1ae0d4b2ad0ef9334ba01f4af4bc35dde927d11a
jiyoonlee <im.lizhilun@gmail.com>
2024-03-12 02:31:57 +0900

    메인 sub-title 추가

--- src/components/MainTitle/MainTitle.js
+++ src/components/MainTitle/MainTitle.js
@@ -4,11 +4,14 @@ import './MainTitle.scss';
 const MainTitle = () => {
   return (
     <>
-      <div className='title-wrap'>
+      <div className='title-wrapper'>
         <h1>
           <span>Front-end</span>
           <span>Jiyoon Lee</span>
         </h1>
+        <span className='sub-title'>
+          배움을 즐거워하는 개발자 이지윤입니다 :)
+        </span>
       </div>
     </>
   );
